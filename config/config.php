eff48d189d9fd1c90f50ebfd4ed9a29e5812fa99
Kirsten Roschanski <kirsten@kat42.de>
2017-03-30 11:01:28 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -21,6 +21,6 @@ $GLOBALS['BE_MOD']['isotope']['iso_orders']['export_bank']   = array('Isotope\Co
 
 if (TL_MODE == 'BE')
 {
-    $GLOBALS['TL_CSS'][] = \Haste\Util\Debug::uncompressedFile('system/modules/isotope-order_export/assets/css/backend.css|static');
-    $GLOBALS['TL_JAVASCRIPT'][] = \Haste\Util\Debug::uncompressedFile('system/modules/isotope-order_export/assets/js/backend.js|static');
+    $GLOBALS['TL_CSS'][] = \Haste\Util\Debug::uncompressedFile('system/modules/isotope_export/assets/css/backend.css|static');
+    $GLOBALS['TL_JAVASCRIPT'][] = \Haste\Util\Debug::uncompressedFile('system/modules/isotope_export/assets/js/backend.js|static');
 }
